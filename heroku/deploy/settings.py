010a33e1df5e219da2630f84a3198850149991fa
Lukasz Malucha <lucasmalucha@gmail.com>
2020-08-01 10:14:35 +0100

    heroku

--- heroku/deploy/settings.py
+++ heroku/deploy/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u2$gjhlygyx=_)q42*h3o_y7jf^dsh-z%%#6t^))x)zz5i96py'
+if "SECRET_KEY" in os.environ:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+else:
+    SECRET_KEY = "TestDjango"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-test.herokuapp.com', 'www.example.com']
 
 
 # Application definition
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- readme.md
+++ readme.md
@@ -1,2 +1,15 @@
 ### DJANGO DEPLOYMENT
 
+#### Heroku
+
+cd myapp\
+git init\
+git add .\
+git commit -m "My first commit"\
+git push heroku master
+
+heroku domains:add www.example.com
+heroku domains:wait 'www.example.com'
+
+openssl genrsa -des3 -out server.pass.key 2048
+openssl rsa -in server.pass.key -out server.key
